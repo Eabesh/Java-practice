71e958324b009a12df420a3f6d23ecf07f4d56e2
mamtay <mamta.nick@gmail.com>
2016-11-16 08:29:22 +0530

    added solutions

---
+++ src/main/java/greedy/algorithms/FracionalKnapSack.java
@@ -0,0 +1,12 @@
+package greedy.algorithms;
+
+public class FracionalKnapSack {
+
+    /**
+     *Given weights and values of n items, we need put these items in a knapsack of capacity W to get the maximum
+     * total value in the knapsack.
+     * In Fractional Knapsack, we can break items for maximizing the total value of knapsack. This problem in which we
+     * can break item also called fractional knapsack problem.
+     */
+
+}
---
+++ src/test/java/greedy/algorithms/ActivitySelectionProblemTest.java
@@ -0,0 +1,6 @@
+package greedy.algorithms;
+
+public class ActivitySelectionProblemTest {
+
+
+}
