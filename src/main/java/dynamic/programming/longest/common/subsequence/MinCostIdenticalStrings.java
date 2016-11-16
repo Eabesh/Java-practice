ca9523530a0c59e8072de4a20c11a0a12e9c5d74
mamtay <mamta.nick@gmail.com>
2016-11-16 14:44:28 +0530

    added solutions

--- src/main/java/dynamic/programming/longest/common/subsequence/MinCostIdenticalStrings.java
+++ src/main/java/dynamic/programming/longest/common/subsequence/MinCostIdenticalStrings.java
@@ -7,6 +7,19 @@ public class MinCostIdenticalStrings {
      * given two strings identical. We can delete characters from both the strings.
      * The cost of deleting a character from string X is costX and from Y is costY. Cost of removing all characters
      * from a string is same.
+     * Minimum Cost to make strings identical = costX * (m - len_LCS) +
+     costY * (n - len_LCS).
+
+     m ==> Length of string X
+     m ==> Length of string Y
+     len_LCS ==> Length of LCS Of X and Y.
+     costX ==> Cost of removing a character from X[]
+     costY ==> Cost of removing a character from Y[]
      */
+
+    int minCost(int costX, int costY, String s1, String s2) {
+        int lcs = new LCS().lcs(s1,s2);
+        return costX * (s1.length() - lcs) + costY * (s2.length() - lcs);
+    }
 
 }
---
+++ src/test/java/dynamic/programming/longest/common/subsequence/KPalindromeTest.java
@@ -0,0 +1,15 @@
+package dynamic.programming.longest.common.subsequence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KPalindromeTest {
+
+    @Test
+    public void isKPalTest() {
+        String s = "abcdecba";
+        int k = 1;
+        assertEquals(true,new KPalindrome().isKPal(s,1));
+    }
+}
