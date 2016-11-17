6a0369b044db65ced6302fdd3612eb5e96146b73
mamtay <mamta.nick@gmail.com>
2016-11-17 18:09:18 +0530

    added solutions

--- src/main/java/data/structures/arrays/ArrayExamples.java
+++ src/main/java/data/structures/arrays/ArrayExamples.java
@@ -70,10 +70,12 @@ public class ArrayExamples {
     }
 
     /**
+     * 5.
+     * Problem: Find the Missing Number. You are given a list of n-1 integers and these integers are in the range
+     * of 1 to n. There are no duplicates in list. One of the integers is missing in the list.
+     * Write an efficient code to find the missing integer.
      *
-     * Problem:
-     *
-     * Solution:
+     * Solution: Use sum formulae or XOR operator.
      */
 
     /**
