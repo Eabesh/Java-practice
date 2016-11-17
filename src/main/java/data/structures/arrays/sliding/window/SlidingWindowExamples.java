bbca9e584ce1968c992f3535b93373837864e815
mamtay <mamta.nick@gmail.com>
2016-11-17 08:47:56 +0530

    added solutions

--- src/main/java/data/structures/arrays/sliding/window/SlidingWindowExamples.java
+++ src/main/java/data/structures/arrays/sliding/window/SlidingWindowExamples.java
@@ -11,6 +11,23 @@ public class SlidingWindowExamples {
      Output: Maximum average subarray of length 4 begins
      at index 1.
      */
+    int maxAverage(int[] array, int k) {
+        int currMax = 0;
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < k; i++) currMax += array[i];
+        int windowL;
+        int windowR = k - 1;
+        for (windowL = 0; windowL <= array.length - k; windowL++) {
+            currMax -= array[windowL];
+            currMax += array[++windowR];
+            if (currMax > max) {
+                max = currMax;
+                index = windowL;
+            }
+        }
+        return index;
+    }
 
     /**
      * 143. Find maximum average subarray of k length.
@@ -20,5 +37,6 @@ public class SlidingWindowExamples {
      * 141. Given a binary array and an integer m, find the position of zeroes flipping which creates maximum
      * number of consecutive 1s in array.
      */
+
 
 }
