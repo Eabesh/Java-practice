2483245f4d5d970427ecdf2a57b9e74af2d5b6bf
mamtay <mamta.nick@gmail.com>
2016-11-17 09:46:52 +0530

    added solutions

--- src/main/java/cormen/sorting/BubbleSort.java
+++ src/main/java/cormen/sorting/BubbleSort.java
@@ -4,8 +4,8 @@ public class BubbleSort {
 
     public void sort(int[] array) {
         for(int i = 0; i < array.length;  i++) {
-            for(int j = i+1; j < array.length;  j++) {
-                if(array[i] > array[j]) swap(array,i,j);
+            for(int j = 0; j < array.length - i - 1;  j++) {
+                if(array[j] > array[j+1]) swap(array,j,j+1);
             }
         }
     }
