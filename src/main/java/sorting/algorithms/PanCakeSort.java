663734a80ee0bf7045e9e048e7cead9480e2964b
mamtay <mamta.nick@gmail.com>
2016-11-23 14:06:41 +0530

    added solutions

--- src/main/java/sorting/algorithms/PanCakeSort.java
+++ src/main/java/sorting/algorithms/PanCakeSort.java
@@ -11,9 +11,45 @@ public class PanCakeSort {
      *
      * Solution: Start from current size equal to n and reduce current size by one while it’s greater than 1.
      * Let the current size be curr_size. Do following for every curr_size
-     * 1) Find index of the maximum element in arr[0..curr_szie-1]. Let the index be ‘mi’
+     * 1) Find index of the maximum element in arr[0..curr_size-1]. Let the index be ‘mi’
      * b) Call flip(arr, mi)
      * c) Call flip(arr, curr_size-1)
      */
+
+    void panCakeSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int maxIndex = getMax(array, array.length - i);
+            flip(array, 0, maxIndex);
+            flip(array, 0, array.length - i);
+        }
+    }
+
+    private int getMax(int[] array, int length) {
+        int max = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        for (int i = 0; i < length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    private void flip(int[] array, int start, int end) {
+        while (start < end) {
+            swap(array, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] array, int i, int j) {
+        if (i != j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
 }
