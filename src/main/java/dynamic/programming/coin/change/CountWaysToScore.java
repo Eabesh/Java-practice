6800ea0c9cd649b9410ca9784adf913dbe0a0f77
mamtay <mamta.nick@gmail.com>
2016-11-24 17:03:33 +0530

    added solutions

--- src/main/java/dynamic/programming/coin/change/CountWaysToScore.java
+++ src/main/java/dynamic/programming/coin/change/CountWaysToScore.java
@@ -1,10 +1,11 @@
 package dynamic.programming.coin.change;
 
-public class CountWaysToScore {
+class CountWaysToScore {
 
     /**
-     * Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n,
+     * Problem: Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n,
      * find number of ways to reach the given score.
+     *
      * Time Complexity : O(n)
      */
     int count(int[] scores, int n) {
