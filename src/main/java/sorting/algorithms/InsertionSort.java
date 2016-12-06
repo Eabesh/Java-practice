2538b9d98be1565f5898bd5b6ed5c681a9454e6e
pankaj2806 <pankajthakur2806@gmail.com>
2016-12-06 08:27:43 +0530

    added solutions

--- src/main/java/sorting/algorithms/InsertionSort.java
+++ src/main/java/sorting/algorithms/InsertionSort.java
@@ -5,9 +5,10 @@ class InsertionSort {
     public int[] sort(int[] array) {
         for(int i = 1; i < array.length; i++) {
             int key = array[i];
-            int j;
-            for (j = i-1; j > -1; j--){
+            int j = i - 1;
+            while (j >= 0) {
                 if (array[j] > key) array[j+1] = array[j];
+                j--;
             }
             array[j+1] = key;
         }
