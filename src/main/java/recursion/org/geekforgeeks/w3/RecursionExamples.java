37fe6a027c4b33b090e5e48f3ced2220468ea588
abhay0609 <abhay0609@gmail.com>
2017-04-18 10:46:04 +0530

    added more problems

--- src/main/java/recursion/org/geekforgeeks/w3/RecursionExamples.java
+++ src/main/java/recursion/org/geekforgeeks/w3/RecursionExamples.java
@@ -17,12 +17,11 @@ class RecursionExamples {
   int getExponentEfficient(int base, int exp) {
     if (exp == 0) return 1;
     else {
-      int half = base * getExponentEfficient((base, exp - 1);
+      int half = base * getExponentEfficient(base, exp - 1);
       if (exp % 2 == 0) return half * half;
       else return base * half * half;
     }
   }
-
 
   /**
    * 2.
