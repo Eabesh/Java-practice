32f4148fb3c0ddae00832a9a3be5b8257eed060a
abhay0609 <abhay0609@gmail.com>
2017-05-02 13:39:23 +0530

    added dp examples

---
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -0,0 +1,264 @@
+package algorithms.dynamic.programming;
+
+/**
+ * Created by abhay on 02/05/17.
+ */
+public class DPExamples {
+
+  /**
+   * GeeksForGeeks Problems
+   */
+
+  /**
+   * 1.
+   * Problem: Largest Sum Contiguous Subarray.
+   * Write an efficient C program to find the sum of contiguous
+   * sub array within a one-dimensional array of numbers which has
+   * the largest sum.
+   * Solution: Kadane’s Algorithm:
+   * Initialize:
+   * maxSoFar = 0, currMax = 0;
+   * if (currMax < 0) currMax = 0;
+   * if (maxSoFar < currMax) maxSoFar = currMax
+   * return maxSoFar
+   */
+
+
+  /**
+   * 2.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 7.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 8.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 9.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 10.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 11.
+   * Problem:
+   * Solution:
+   */
+
+
+  /**
+   * 12.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 13.
+   * Problem:
+   * Solution:
+   */
+
+
+  /**
+   * 14.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 15.
+   * Problem:
+   * Solution:
+   */
+
+
+  /**
+   * 16.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 17.
+   * Problem:
+   * Solution:
+   */
+
+
+  /**
+   * 18.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 19.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 20.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 21.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 22.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 23.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 24.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 25.
+   * Problem:
+   * Solution:
+   */
+
+
+  /**
+   * 26.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 27.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 28.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 29.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 30.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 31.
+   * Problem:
+   * Solution:
+   */
+  /**
+   * 32.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 33.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 34.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 35.
+   * Problem:
+   * Solution:
+   */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
