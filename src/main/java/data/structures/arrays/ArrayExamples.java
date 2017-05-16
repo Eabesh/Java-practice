478ef6a93152c8434792c9de4f1ed14d6b13959a
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-05-16 10:26:33 +0530

    added code

--- src/main/java/data/structures/arrays/ArrayExamples.java
+++ src/main/java/data/structures/arrays/ArrayExamples.java
@@ -322,6 +322,31 @@ public class ArrayExamples {
    */
 
   /**
+   * 41.
+   * Problem: Given an array arr[], find the maximum j – i such that arr[j] > arr[i].
+   * Solution : Create LeftMin and RightMax
+   * Compare both array at each index and update maxDiff.
+   * 34 1 2 10
+   */
+  int maxDiffArray(int[] arr) {
+    int maxDiff = -1, x = 0, y = 0;
+    int[] leftMin = new int[arr.length];
+    int[] rightMax = new int[arr.length];
+    leftMin[0] = arr[0];
+    for (int i = 1; i < arr.length; i++) leftMin[i] = Math.min(leftMin[i - 1], arr[i]);
+    rightMax[arr.length - 1] = arr[arr.length - 1];
+    for (int i = arr.length - 2; i >=0; i--) rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+    while (x < arr.length && y < arr.length) {
+      if (arr[x] < arr[y]) {
+        maxDiff = Math.max(maxDiff, y - x);
+        y++;
+      }
+      else x++;
+    }
+    return maxDiff;
+  }
+
+  /**
    * 49.
    * Problem: Find a Fixed Point in a given array.
    * Given an array of n distinct integers sorted in ascending order, write a function that returns a Fixed Point
@@ -626,11 +651,81 @@ public class ArrayExamples {
    */
 
   /**
-   * 139. Convert array into Zig-Zag fashion.
+   * 136.
+   * Problem: Trapping Rain Water
+   * Given n non-negative integers representing an elevation map where the width of each bar
+   * is 1, compute how much water it is able to trap after raining.
+   * For example, given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+   */
+  int rainWater(int[] array) {
+    int[] left = new int[array.length];
+    left[0] = array[0];
+    for (int i = 1; i < array.length; i++) left[i] = Math.max(left[i-1], array[i]);
+    int[] right = new int[array.length];
+    right[array.length - 1] = array[array.length - 1];
+    for (int i = array.length - 2; i >= 0 ; i--) right[i] = Math.max(right[i+1], array[i]);
+
+    int water = 0;
+    for(int i = 0; i < array.length; i++) water += Math.min(left[i], right[i]) - array[i];
+    return water;
+  }
+
+  /**
+   * 138.
+   * Problem: Find maximum average subarray of k length
+   * Given an array with positive and negative numbers, find the maximum average subarray of given length.
+   * Example:
+   * Input: arr[] = {1, 12, -5, -6, 50, 3}, k = 4
+   * Output: Maximum average subarray of length 4 begins at index 1.
+   */
+  int maxAverage(int[] array, int k) {
+    int currMax = Arrays.stream(array).limit(k).sum();
+    int max = 0; int index = 0;
+    int windowL;
+    int windowR = k - 1;
+    for (windowL = 0; windowL <= array.length - k; windowL++) {
+      currMax -= array[windowL];
+      currMax += array[++windowR];
+      if (currMax > max) {
+        max = currMax;
+        index = windowL;
+      }
+    }
+    return index;
+  }
+
+  /**
+   * 139.
+   * Problem: Convert array into Zig-Zag fashion.
    * Given an array of distinct elements, rearrange the elements of array in zig-zag fashion in O(n) time.
    * The converted array should be in form a < b > c < d > e < f.
-   *
    * Solution: Scan left to right and swap at odd and even places according to condition given above.
+   */
+
+  /**
+   * 141.
+   * Problem: Given a binary array and an integer m, find the position of zeroes flipping which creates maximum
+   * number of consecutive 1s in array.
+   */
+
+  int maxLength(int[] array, int k) {
+    int windowR = 0, windowL = 0, maxLength = 0, numZeroes = 0;
+    while (windowR < array.length) {
+      if (numZeroes <= k) {
+        if (windowR < array.length && array[windowR] == 0) numZeroes++;
+        windowR++;
+      }
+      if (numZeroes > k) {
+        if (windowL < array.length && array[windowL] == 0) numZeroes--;
+        windowL++;
+      }
+      maxLength = Math.max(maxLength, windowR - windowL);
+    }
+    return maxLength;
+  }
+
+  /**
+   * 143. Find minimum average subarray of k length.
    */
 
   /**
@@ -775,6 +870,23 @@ public class ArrayExamples {
       max = Math.max(array[j], max);
     }
     return max - min;
+  }
+
+  /**
+   * Candies
+   * There are N children standing in a line. Each child is assigned a rating value.
+   * You are giving candies to these children subjected to the following requirements:
+   * 1. Each child must have at least one candy.
+   * 2. Children with a higher rating get more candies than their neighbors.
+   */
+  int countCandies(int[] ratings) {
+    int[] candies = new int[ratings.length];
+    Arrays.fill(candies,1);
+    for (int i = 1; i < candies.length; i++)
+      if (ratings[i - 1] < ratings[i]) candies[i] = Math.max(candies[i-1] + 1, candies[i]);
+    for (int i = ratings.length - 2; i >= 0; i--)
+      if (ratings[i] > ratings[i+1]) candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+    return Arrays.stream(candies).sum();
   }
 
 }
