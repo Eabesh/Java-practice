8152dba2159882e9e25fdb85d6f8bc06a2a54956
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-05-25 11:01:10 +0530

    added code

--- src/main/java/data/structures/arrays/ArrayExamples.java
+++ src/main/java/data/structures/arrays/ArrayExamples.java
@@ -873,7 +873,7 @@ public class ArrayExamples {
   }
 
   /**
-   * Candies
+   * Problem: Candies distribution
    * There are N children standing in a line. Each child is assigned a rating value.
    * You are giving candies to these children subjected to the following requirements:
    * 1. Each child must have at least one candy.
