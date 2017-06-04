73a53344566f7abc50245f73bde84c624ff209fa
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-04 11:47:57 +0530

    added code

--- src/test/java/data/structures/linkedlists/ListExampleTest.java
+++ src/test/java/data/structures/linkedlists/ListExampleTest.java
@@ -7,54 +7,61 @@ import static org.junit.Assert.assertEquals;
 
 public class ListExampleTest {
 
-    ListNode head;
+  ListNode head;
 
-    @Test
-    public void getNthNodeTest() {
-        getList(10);
-        assertEquals(5,new ListExample().getNthNode(head,5).data);
+  @Test
+  public void getNthNodeTest() {
+    createList(10);
+    assertEquals(5,new ListExample().getNthNode(head,5).data);
+  }
+
+  @Test
+  public void getNthNodeRecTest() {
+    createList(10);
+    assertEquals(5,new ListExample().getNthNodeRec(head,5).data);
+  }
+
+  @Test
+  public void getReverseKNode() {
+    createList(10);
+    ListNode newHead = new ListExample().reverseKNodes(head, 5, 10);
+    System.out.print(newHead.data);
+
+  }
+
+  @Test
+  public void reverseListTest() {
+    createList(10);
+    assertEquals(10,new ListExample().reverseList(head).data);
+  }
+
+  @Test
+  public void reverseListRecTest() {
+    createList(20);
+    assertEquals(20,new ListExample().reverseListRec(head).data);
+  }
+
+  private void createList(int n) {
+    ListNode current = head;
+    for (int i = 1; i <= n; i++) {
+      ListNode node = getNode(i);
+      if (isFirstNode(node)) {
+        head = node;
+        current = node;
+      }
+      else {
+        current.next = node;
+        current = current.next;
+      }
     }
+  }
 
-    @Test
-    public void getNthNodeRecTest() {
-        getList(10);
-        assertEquals(5,new ListExample().getNthNodeRec(head,5).data);
-    }
+  private boolean isFirstNode(ListNode listNode) {
+    return listNode.data == 1;
+  }
 
-    @Test
-    public void getReverseKNode() {
-        getList(10);
-        ListNode newHead = new ListExample().reverseKNodes(head, 5, 10);
-        System.out.print(newHead.data);
-
-    }
-
-    @Test
-    public void reverseListTest() {
-        getList(10);
-        assertEquals(10,new ListExample().reverseList(head).data);
-    }
-
-    @Test
-    public void reverseListRecTest() {
-        getList(20);
-        assertEquals(20,new ListExample().reverseListRec(head).data);
-    }
-
-    private void getList(int n) {
-        ListNode current = head;
-        for (int i = 1; i <= n; i++) {
-            ListNode node = new ListNode(i);
-            if (i == 1) {
-                head = node;
-                current = node;
-            }
-            else {
-                current.next = node;
-                current = current.next;
-            }
-
-        }
-    }
+  private ListNode getNode(int data) {
+    return new ListNode(data);
+  }
 
 }
