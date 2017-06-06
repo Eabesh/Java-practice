85c17ce88d668b3ed966cefa8947848daa8886da
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-06 08:41:08 +0530

    added code

--- src/main/java/data/structures/queue/QueueExamples.java
+++ src/main/java/data/structures/queue/QueueExamples.java
@@ -3,6 +3,55 @@ package data.structures.queue;
 public class QueueExamples {
 
   /**
+   * 1.
+   * Problem: Introduction and Array Implementation
+   * Solution:  Queue is a linear structure which follows a particular order in which the operations are performed. The
+   * order is First In First Out (FIFO).
+   * Mainly the following four basic operations are performed on queue:
+   * 1. Enqueue: Adds an item to the queue. If the queue is full, then it is said to be an Overflow condition.
+   * 2. Dequeue: Removes an item from the queue. The items are popped in the same order in which they are pushed. If
+   * the queue is empty, then it is said to be an Underflow condition.
+   * 3. Front: Get the front item from queue.
+   * 4. Rear: Get the last item from queue.
+   * Applications:
+   * Queue is used when things don’t have to be processed immediately, but have to be processed in First InFirst Out
+   * order like Breadth First Search. This property of Queue makes it also useful in following kind of scenarios.
+   * 1) When a resource is shared among multiple consumers. Examples include CPU scheduling, Disk Scheduling.
+   * 2) When data is transferred asynchronously (data not necessarily received at same rate as sent) between two
+   * processes. Examples include IO Buffers, pipes, file IO, etc.
+   */
+
+  /**
+   * 1.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   * Solution:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   * Solution:
+   */
+
+  /**
    * 6.
    * Problem: Implement Queue using Stacks.
    * Solution:
