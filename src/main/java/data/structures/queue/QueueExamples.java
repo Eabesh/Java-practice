da7f981f3c84c2a1a36c21fdf440a7c9be1c5aa5
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-06 08:29:14 +0530

    added code

--- src/main/java/data/structures/queue/QueueExamples.java
+++ src/main/java/data/structures/queue/QueueExamples.java
@@ -2,21 +2,18 @@ package data.structures.queue;
 
 public class QueueExamples {
 
-    /**
-     * 6.
-     * Problem: Implement Queue using Stacks.
-     *
-     * Solution:
-     * By making deQueue operation costly
-     * enQueue(q,  x)
-     * 1) Push x to stack1 (assuming size of stacks is unlimited).
-     *
-     * deQueue(q)
-     *      1) If both stacks are empty then error.
-     *      2) If stack2 is empty
-     *          While stack1 is not empty, push everything from stack1 to stack2.
-     *      3) Pop the element from stack2 and return it.
-     */
-
+  /**
+   * 6.
+   * Problem: Implement Queue using Stacks.
+   * Solution:
+   * By making deQueue operation costly:
+   * enQueue(q,  x)
+   *   1) Push x to stack1 (assuming size of stacks is unlimited).
+   * deQueue(q)
+   *   1) If both stacks are empty then error.
+   *   2) If stack2 is empty
+   *      While stack1 is not empty, push everything from stack1 to stack2.
+   *   3) Pop the element from stack2 and return it.
+   */
 
 }
