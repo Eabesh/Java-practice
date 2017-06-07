4be2176511e91d6d91a7b98c0cac731cdab89a6b
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-07 23:20:31 +0530

    added code

--- src/main/java/algorithms/sorting/BubbleSort.java
+++ src/main/java/algorithms/sorting/BubbleSort.java
@@ -4,7 +4,7 @@ class BubbleSort {
 
   public void sort(int[] array) {
     for(int i = 0; i < array.length; i++) {
-      for(int j = 0; j < array.length - i - 1;  j++) {
+      for(int j = 0; j < array.length - 1 - i;  j++) {
         if(array[j] > array[j+1]) swap(array,j,j+1);
       }
     }
