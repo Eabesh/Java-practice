68d11e5cef6b43044f3e517ee643054d9a1a284b
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-07 23:33:38 +0530

    added code

--- src/main/java/algorithms/sorting/InsertionSort.java
+++ src/main/java/algorithms/sorting/InsertionSort.java
@@ -6,8 +6,8 @@ class InsertionSort {
     for(int i = 1; i < array.length; i++) {
       int key = array[i];
       int j = i - 1;
-      while (j >= 0) {
-        if (array[j] > key) array[j+1] = array[j];
+      while (j >= 0 && array[j] > key) {
+        array[j+1] = array[j];
         j--;
       }
       array[j+1] = key;
