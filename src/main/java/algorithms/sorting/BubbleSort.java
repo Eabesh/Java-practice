efcf3b8753967ee843d78545b0cb2f151c5e5c42
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-07 23:19:06 +0530

    added code

--- src/main/java/algorithms/sorting/BubbleSort.java
+++ src/main/java/algorithms/sorting/BubbleSort.java
@@ -3,17 +3,17 @@ package algorithms.sorting;
 class BubbleSort {
 
   public void sort(int[] array) {
-      for(int i = 0; i < array.length; i++) {
-          for(int j = 0; j < array.length - i - 1;  j++) {
-              if(array[j] > array[j+1]) swap(array,j,j+1);
-          }
+    for(int i = 0; i < array.length; i++) {
+      for(int j = 0; j < array.length - i - 1;  j++) {
+        if(array[j] > array[j+1]) swap(array,j,j+1);
       }
+    }
   }
 
   private void swap(int[] a, int i, int j) {
-      int temp = a[i];
-      a[i] = a[j];
-      a[j] = temp;
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
   }
 
 }
