5a6c938c9267c7131b00fb09e3d6fa2466b2fc11
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-10 15:28:53 +0530

    added code

--- src/main/java/algorithms/sorting/SortingExamples.java
+++ src/main/java/algorithms/sorting/SortingExamples.java
@@ -14,9 +14,9 @@ public class SortingExamples {
   }
 
   private void swap(int[] a, int i, int j) {
-    int temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+    a[i] = a[i] ^ a[j];
+    a[j] = a[i] ^ a[j];
+    a[i] = a[i] ^ a[j];
   }
 
   /**
