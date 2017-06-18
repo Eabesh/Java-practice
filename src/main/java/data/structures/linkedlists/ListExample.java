b40f7273d3a19ad61f51a78a6d6e03929caf054c
abhay0609 <abhay0609@gmail.com>
2017-06-19 01:15:01 +0530

    refactoring

--- src/main/java/data/structures/linkedlists/ListExample.java
+++ src/main/java/data/structures/linkedlists/ListExample.java
@@ -648,7 +648,13 @@ class ListExample {
    * 49.
    * Problem: Sort a linked list of 0s, 1s and 2s
    * Solution:
+   * 1) Traverse the list and count the number of 0s, 1s and 2s. Let the
+   * counts be n1, n2 and n3 respectively.
+   * 2) Traverse the list again, fill the first n1 nodes with 0, then n2
+   * nodes with 1 and finally n3 nodes with 2.
    */
+  void sortList(ListNode head) {
+  }
 
   /**
    * 50.
