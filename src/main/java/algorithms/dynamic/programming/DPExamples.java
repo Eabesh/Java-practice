cd7a70e8a4cfb14af7b29d77a07383d4dccad051
abhay0609 <abhay0609@gmail.com>
2017-06-20 17:28:08 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -55,8 +55,8 @@ public class DPExamples {
   /**
    * 2.
    * Problem: Ugly Numbers
-   * Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The
-   * sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11
+   * Ugly numbers are numbers whose only prime factors are 2, 3 or 5.
+   * Thesequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11
    * ugly numbers. By convention, 1 is included.
    * Given a number n, the task is to find n’th Ugly number.
    * Input  : n = 7
@@ -74,6 +74,7 @@ public class DPExamples {
   /**
    * 3.
    * Problem: Maximum size square sub-matrix with all 1s
+   * Given a binary matrix, find out the maximum size square sub-matrix with all 1s.
    * Solution:
    */
 
