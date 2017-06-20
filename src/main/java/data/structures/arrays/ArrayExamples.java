69b8f643400b062fe4fea6ad0225ba8ef6914619
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-20 08:50:37 +0530

    added code

--- src/main/java/data/structures/arrays/ArrayExamples.java
+++ src/main/java/data/structures/arrays/ArrayExamples.java
@@ -160,10 +160,8 @@ public class ArrayExamples {
     int arr2Last = arr2.length - arr1.length - 1;
     int last = arr2.length - 1;
     while (arr2Last >= 0 && arr1Last >=0) {
-      if (arr2[arr2Last] > arr1[arr1Last])
-        arr2[last--] = arr2[arr2Last--];
-      else
-        arr2[last--] = arr1[arr1Last--];
+      if (arr2[arr2Last] > arr1[arr1Last]) arr2[last--] = arr2[arr2Last--];
+      else arr2[last--] = arr1[arr1Last--];
     }
     while(arr1Last >= 0) arr2[last--] = arr1[arr1Last--];
     return arr2;
