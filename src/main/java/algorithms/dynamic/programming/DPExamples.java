b45dec6be68c2057743439a9e5bc13d8dc29de0e
abhay0609 <abhay0609@gmail.com>
2017-06-21 11:04:40 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -182,8 +182,16 @@ public class DPExamples {
    * Given two sequences, find the length of longest subsequence present in both of
    * them. A subsequence is a sequence that appears in the same relative order, but
    * not necessarily contiguous.
-   * Solution:
+   * LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+   * Solution: Recursive Complexity = O(2^n).
    */
+  int findLongestCommonSeq(String str1, String str2, int i, int j) {
+    if (i == str1.length() || j == str2.length()) return 0;
+    else if (str1.charAt(i) == str2.charAt(j)) return 1 + findLongestCommonSeq(str1, str2, i + 1 , j + 1);
+    else return Math.max(findLongestCommonSeq(str1, str2, i + 1, j), findLongestCommonSeq(str1, str2, i, j + 1));
+  }
+
+  int findLCSeq(String str1, String str2, int )
 
   /**
    * 9.
