cb57bc4ad759c2707816690d0a711d26f7c83a99
abhay0609 <abhay0609@gmail.com>
2017-06-21 10:50:05 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -179,6 +179,9 @@ public class DPExamples {
   /**
    * 8.
    * Problem: Dynamic Programming | Set 4 (Longest Common Subsequence)
+   * Given two sequences, find the length of longest subsequence present in both of
+   * them. A subsequence is a sequence that appears in the same relative order, but
+   * not necessarily contiguous.
    * Solution:
    */
 
