9fe637ca3b475755b1af8b51b5dbae6e6f3d943b
abhay0609 <abhay0609@gmail.com>
2017-06-22 18:16:18 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1430,6 +1430,17 @@ public class DPExamples {
   /**
    * 185.
    * Problem: Subset with sum divisible by m
+   * Given a set of non-negative distinct integers, and a value m, determine if there is a subset of the given set with
+   * sum divisible by m.
+   * Input Constraints
+   * Size of set i.e., n <= 1000000, m <= 1000
+   * Examples:
+   * Input : arr[] = {3, 1, 7, 5};
+   * m = 6;
+   * Output : YES
+   * Input : arr[] = {1, 6};
+   * m = 5;
+   * Output : NO
    * Solution:
    */
 
