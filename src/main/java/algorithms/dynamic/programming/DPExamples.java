08a0056ead25d6fb4e47a6124f9b82bf9be50182
abhay0609 <abhay0609@gmail.com>
2017-06-22 10:47:55 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -444,9 +444,30 @@ public class DPExamples {
   /**
    * 35.
    * Problem: Dynamic Programming | Set 28 (Minimum insertions to form a palindrome)
+   * Given a string, find the minimum number of characters to be inserted to convert it to palindrome.
+   * Before we go further, let us understand with few examples:
+   * ab: Number of insertions required is 1. bab
+   * aa: Number of insertions required is 0. aa
+   * abcd: Number of insertions required is 3. dcbabcd
+   * abcda: Number of insertions required is 2. adcbcda which is same as number of insertions in the substring bcd(Why?).
+   * abcde: Number of insertions required is 4. edcbabcde
    * Solution:
+   * Let the input string be str[l……h]. The problem can be broken down into three parts:
+   * 1. Find the minimum number of insertions in the substring str[l+1,…….h].
+   * 2. Find the minimum number of insertions in the substring str[l…….h-1].
+   * 3. Find the minimum number of insertions in the substring str[l+1……h-1].
+   * Recursive Solution
+   * The minimum number of insertions in the string str[l…..h] can be given as:
+   * minInsertions(str[l+1…..h-1]) if str[l] is equal to str[h]
+   * min(minInsertions(str[l…..h-1]), minInsertions(str[l+1…..h])) + 1 otherwise
    */
-
+  int minInsertion(String str, int left, int right) {
+    if (left > right) return Integer.MAX_VALUE;
+    else if (left == right) return 0;
+    else if (left + 1 == right) return (str.charAt(left) == str.charAt(right)) ? 0 : 1;
+    else if (str.charAt(left) == str.charAt(right)) return minInsertion(str, left + 1, right - 1);
+    else return Math.min(minInsertion(str, left + 1, right), minInsertion(str, left, right - 1));
+  }
   /**
    * 35.
    * Problem: Dynamic Programming | Set 29 (Longest Common Substring)
