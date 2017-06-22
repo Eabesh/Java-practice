e343e72248cafd1d96088e0321fa0a88e424576b
abhay0609 <abhay0609@gmail.com>
2017-06-22 23:52:08 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -61,9 +61,8 @@ class DPExamples {
   /**
    * 2.
    * Problem: Ugly Numbers
-   * Ugly numbers are numbers whose only prime factors are 2, 3 or 5.
-   * Thesequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, … shows the first 11
-   * ugly numbers. By convention, 1 is included.
+   * Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
+   * shows the first 11 ugly numbers. By convention, 1 is included.
    * Given a number n, the task is to find n’th Ugly number.
    * Input  : n = 7
    * Output : 8
@@ -73,7 +72,6 @@ class DPExamples {
    * Output : 24
    * Input  : n = 150
    * Output : 5832
-
    * Solution:
    */
 
