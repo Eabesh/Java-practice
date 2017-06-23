dcfc880ac687a462177b01ae87bca431b4f5c130
abhay0609 <abhay0609@gmail.com>
2017-06-23 13:32:41 +0530

    refactoring

--- src/main/java/data/structures/stack/StackExamples.java
+++ src/main/java/data/structures/stack/StackExamples.java
@@ -4,4 +4,182 @@ package data.structures.stack;
  * Created by abhay on 23/06/17.
  */
 public class StackExamples {
+
+  /**
+   * 1.
+   * Problem: Implement Queue using Stacks
+   */
+
+  /**
+   * 2.
+   * Problem: Check for balanced parentheses in an expression
+   */
+
+
+  /**
+   * 3.
+   * Problem: Reverse a stack using recursion
+   */
+
+
+  /**
+   * 4.
+   * Problem: Next Greater Element
+   */
+
+
+  /**
+   * 5.
+   * Problem: Spaghetti Stack
+   */
+
+
+  /**
+   * 6.
+   * Problem: Design and Implement Special Stack Data Structure | Added Space Optimized Version
+   */
+
+  /**
+   * 7.
+   * Problem: Implement two stacks in an array
+   */
+
+
+  /**
+   * 8.
+   * Problem: The Celebrity Problem
+   */
+
+
+  /**
+   * 9.
+   * Problem: Implement Stack using Queues
+   */
+
+
+  /**
+   * 10.
+   * Problem: The Stock Span Problem
+   */
+
+
+
+
+
+  /**
+   * 11.
+   * Problem: Iterative Postorder Traversal | Set 1 (Using Two Stacks)
+   */
+
+  /**
+   * 12.
+   * Problem: Iterative Postorder Traversal | Set 2 (Using One Stack)
+   */
+
+
+  /**
+   * 13.
+   * Problem: Merge Overlapping Intervals
+   */
+
+
+  /**
+   * 14.
+   * Problem: Stack | Set 2 (Infix to Postfix)
+   */
+
+
+  /**
+   * 15.
+   * Problem: Largest Rectangular Area in a Histogram | Set 2
+   */
+
+
+  /**
+   * 16.
+   * Problem:
+   */
+
+  /**
+   * 17.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
+
+  /**
+   * 1.
+   * Problem:
+   */
+
 }
