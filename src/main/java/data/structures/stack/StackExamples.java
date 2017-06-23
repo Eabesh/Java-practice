890f77533f91ff2dc7ad2d5d05186b0fcc130be2
abhay0609 <abhay0609@gmail.com>
2017-06-23 13:51:01 +0530

    refactoring

--- src/main/java/data/structures/stack/StackExamples.java
+++ src/main/java/data/structures/stack/StackExamples.java
@@ -92,6 +92,10 @@ public class StackExamples {
   /**
    * 15.
    * Problem: Largest Rectangular Area in a Histogram | Set 2
+   * Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a number
+   * of contiguous bars. For simplicity, assume that all bars have same width and the width is 1 unit.
+   * For example, consider the following histogram with 7 bars of heights {6, 2, 5, 4, 5, 1, 6}. The largest possible
+   * rectangle possible is 12
    */
 
 
