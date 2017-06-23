c84293b251fdafef38fad36c42ef22fbff8cf96d
abhay0609 <abhay0609@gmail.com>
2017-06-23 13:38:39 +0530

    refactoring

--- src/main/java/data/structures/stack/StackExamples.java
+++ src/main/java/data/structures/stack/StackExamples.java
@@ -97,89 +97,174 @@ public class StackExamples {
 
   /**
    * 16.
-   * Problem:
+   * Problem: Expression Evaluation
    */
 
   /**
    * 17.
-   * Problem:
+   * Problem: Design a stack with operations on middle element
+   */
+
+
+  /**
+   * 18.
+   * Problem: Print ancestors of a given binary tree node without recursion
+   */
+
+
+  /**
+   * 19.
+   * Problem: Stack | Set 3 (Reverse a string using stack)
+   */
+
+
+  /**
+   * 20.
+   * Problem: C Program for Tower of Hanoi
+   */
+
+
+  /**
+   * 21.
+   * Problem: Stack | Set 4 (Evaluation of Postfix Expression)
+   */
+
+  /**
+   * 22.
+   * Problem: How to efficiently implement k stacks in a single array?
+   */
+
+
+  /**
+   * 23.
+   * Problem: How to create mergable stack?
+   */
+
+
+  /**
+   * 24.
+   * Problem: Find maximum depth of nested parenthesis in a string
+   */
+
+
+  /**
+   * 25.
+   * Problem: Iterative Tower of Hanoi
+   */
+
+
+
+  /**
+   * 26.
+   * Problem: Find maximum of minimum for every window size in a given array
+   */
+
+  /**
+   * 27.
+   * Problem: Length of the longest valid substring
+   */
+
+
+  /**
+   * 28.
+   * Problem: Sort a stack using recursion
+   */
+
+
+  /**
+   * 29.
+   * Problem: Iterative Depth First Traversal of Graph
+   */
+
+
+  /**
+   * 30.
+   * Problem: Minimum number of bracket reversals needed to make an expression balanced
+   */
+
+  /**
+   * 31.
+   * Problem: Check if a given array can represent Preorder Traversal of Binary Search Tree
+   */
+
+
+
+  /**
+   * 32.
+   * Problem: Form minimum number from given sequence
+   */
+
+  /**
+   * 2.
+   * Problem: Stack Class in Java
    */
 
 
   /**
    * 1.
-   * Problem:
+   * Problem: Design a stack that supports getMin() in O(1) time and O(1) extra space
    */
 
 
   /**
    * 1.
-   * Problem:
+   * Problem: Implement a stack using single queue
    */
 
 
   /**
    * 1.
-   * Problem:
+   * Problem: Find if an expression has duplicate parenthesis or not
    */
 
 
   /**
-   * 1.
-   * Problem:
-   */
-
-  /**
-   * 1.
-   * Problem:
-   */
-
-
-  /**
-   * 1.
-   * Problem:
-   */
-
-
-  /**
-   * 1.
-   * Problem:
-   */
-
-
-  /**
-   * 1.
-   * Problem:
+   * 2.
+   * Problem: Find maximum difference between nearest left and right smaller elements
    */
 
 
 
   /**
-   * 1.
-   * Problem:
+   * 2.
+   * Problem: Find next Smaller of next Greater in an array
    */
 
   /**
-   * 1.
-   * Problem:
-   */
-
-
-  /**
-   * 1.
-   * Problem:
+   * 2.
+   * Problem: Find maximum sum possible equal sum of three stacks
    */
 
 
   /**
    * 1.
-   * Problem:
+   * Problem: Count natural numbers whose all permutation are greater than that number
    */
 
 
   /**
    * 1.
-   * Problem:
+   * Problem: Stack Data Structure (Introduction and Program)
    */
+
+
+  /**
+   * 1.
+   * Problem: How to implement stack using priority queue or heap?
+   */
+  /**
+   * 1.
+   * Problem: Delete consecutive same words in a sequence
+   */
+
+  /**
+   * 1.
+   * Problem: Decode a string recursively encoded as count followed by substring
+   */
+  /**
+   * 1.
+   * Problem: Bubble sort using two Stacks
+   */
+
 
 }
