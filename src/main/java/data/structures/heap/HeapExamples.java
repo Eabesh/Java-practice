2a0c9ada9577853e6990ee14a51cbca522b7cbc1
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-25 22:10:20 +0530

    added code

--- src/main/java/data/structures/heap/HeapExamples.java
+++ src/main/java/data/structures/heap/HeapExamples.java
@@ -2,5 +2,202 @@ package data.structures.heap;
 
 public class HeapExamples {
 
+  /**
+   * 1.
+   * Problem: k largest(or smallest) elements in an array | added Min Heap method.
+   * Solution:
+   */
+
+  /**
+   * 2.
+   * Problem: Applications of Heap Data Structure.
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem: Tournament Tree (Winner Tree) and Binary Heap.
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem: Time Complexity of building a heap.
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem: Median in a stream of integers (running integers).
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem: Sort a nearly sorted (or K sorted) array.
+   * Solution:
+   */
+
+  /**
+   * 7.
+   * Problem: Sort numbers stored on different machines.
+   * Solution:
+   */
+
+  /**
+   * 8.
+   * Problem: Design an efficient data structure for given operations.
+   * Solution:
+   */
+
+  /**
+   * 9.
+   * Problem: Merge k sorted arrays | Set 1.
+   * Solution:
+   */
+
+  /**
+   * 10.
+   * Problem: Connect n ropes with minimum cost.
+   * Solution:
+   */
+
+  /**
+   * 11.
+   * Problem: Print all elements in sorted order from row and column wise sorted matrix.
+   * Solution:
+   */
+
+  /**
+   * 12.
+   * Problem: Binary Heap.
+   * Solution:
+   */
+
+  /**
+   * 13.
+   * Problem: Binomial Heap.
+   * Solution:
+   */
+
+  /**
+   * 14.
+   * Problem: K&#8217;th Smallest/Largest Element in Unsorted Array | Set 1.
+   * Solution:
+   */
+
+  /**
+   * 15.
+   * Problem: Where is Heap Sort used practically?.
+   * Solution:
+   */
+
+  /**
+   * 16.
+   * Problem: K&#8217;th largest element in a stream.
+   * Solution:
+   */
+
+  /**
+   * 17.
+   * Problem: Why is Binary Heap Preferred over BST for Priority Queue?.
+   * Solution:
+   */
+
+  /**
+   * 18.
+   * Problem: How to check if a given array represents a Binary Heap?.
+   * Solution:
+   */
+
+  /**
+   * 19.
+   * Problem: Fibonacci Heap | Set 1 (Introduction).
+   * Solution:
+   */
+
+  /**
+   * 20.
+   * Problem: Check if a given Binary Tree is Heap.
+   * Solution:
+   */
+
+  /**
+   * 21.
+   * Problem: K-ary Heap.
+   * Solution:
+   */
+
+  /**
+   * 22.
+   * Problem: Convert min Heap to max Heap.
+   * Solution:
+   */
+
+  /**
+   * 23.
+   * Problem: Heap using STL C++.
+   * Solution:
+   */
+
+  /**
+   * 24.
+   * Problem: Minimum sum of two numbers formed from digits of an array.
+   * Solution:
+   */
+
+  /**
+   * 25.
+   * Problem: Sum of all elements between k1&#8217;th and k2&#8217;th smallest elements.
+   * Solution:
+   */
+
+  /**
+   * 26.
+   * Problem: Array Representation Of Binary Heap.
+   * Solution:
+   */
+
+  /**
+   * 27.
+   * Problem: Implementation of Binomial Heap.
+   * Solution:
+   */
+
+  /**
+   * 28.
+   * Problem: Rearrange characters in a string such that no two adjacent are same.
+   * Solution:
+   */
+
+  /**
+   * 29.
+   * Problem: Given level order traversal of a Binary Tree, check if the Tree is a Min-Heap.
+   * Solution:
+   */
+
+  /**
+   * 30.
+   * Problem: Applications of Priority Queue.
+   * Solution:
+   */
+
+  /**
+   * 31.
+   * Problem: How to implement stack using priority queue or heap?.
+   * Solution:
+   */
+
+  /**
+   * 32.
+   * Problem: Merge two binary Max Heaps.
+   * Solution:
+   */
+
+  /**
+   * 33.
+   * Problem: Convert BST to Min Heap.
+   * Solution:
+   */
 
 }
