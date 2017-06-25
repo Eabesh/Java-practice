5d33d744a942d7473053dbcd3c6206bec88478c0
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-25 22:24:43 +0530

    added code

---
+++ src/main/java/data/structures/hashing/HashingExamples.java
@@ -0,0 +1,527 @@
+package data.structures.hashing;
+
+public class HashingExamples {
+
+  /**
+   * 1.
+   * Problem: Return maximum occurring character in an input string.
+   * Solution:
+   */
+
+  /**
+   * 2.
+   * Problem: Given an array A[] and a number x, check for pair in A[] with sum as x.
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem: Find whether an array is subset of another array | Added Method 3.
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem: Vertical Sum in a given Binary Tree | Set 1.
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem: Largest subarray with equal number of 0s and 1s.
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem: Find if there is a subarray with 0 sum.
+   * Solution:
+   */
+
+  /**
+   * 7.
+   * Problem: Print a Binary Tree in Vertical Order | Set 2 (Hashmap based Method).
+   * Solution:
+   */
+
+  /**
+   * 8.
+   * Problem: Hashing | Set 1 (Introduction).
+   * Solution:
+   */
+
+  /**
+   * 9.
+   * Problem: Clone a Binary Tree with Random Pointers.
+   * Solution:
+   */
+
+  /**
+   * 10.
+   * Problem: Find the first repeating element in an array of integers.
+   * Solution:
+   */
+
+  /**
+   * 11.
+   * Problem: Print All Distinct Elements of a given integer array.
+   * Solution:
+   */
+
+  /**
+   * 12.
+   * Problem: Length of the largest subarray with contiguous elements | Set 2.
+   * Solution:
+   */
+
+  /**
+   * 13.
+   * Problem: How to check if two given sets are disjoint?.
+   * Solution:
+   */
+
+  /**
+   * 14.
+   * Problem: Check if a given array contains duplicate elements within k distance from each other.
+   * Solution:
+   */
+
+  /**
+   * 15.
+   * Problem: Group multiple occurrence of array elements ordered by first occurrence.
+   * Solution:
+   */
+
+  /**
+   * 16.
+   * Problem: Advantages of BST over Hash Table.
+   * Solution:
+   */
+
+  /**
+   * 17.
+   * Problem: Design a data structure that supports insert, delete, search and getRandom in constant time.
+   * Solution:
+   */
+
+  /**
+   * 18.
+   * Problem: Find number of Employees Under every Employee.
+   * Solution:
+   */
+
+  /**
+   * 19.
+   * Problem: Find Itinerary from a given list of tickets.
+   * Solution:
+   */
+
+  /**
+   * 20.
+   * Problem: Hashing | Set 2 (Separate Chaining).
+   * Solution:
+   */
+
+  /**
+   * 21.
+   * Problem: Count distinct elements in every window of size k.
+   * Solution:
+   */
+
+  /**
+   * 22.
+   * Problem: Hashing | Set 3 (Open Addressing).
+   * Solution:
+   */
+
+  /**
+   * 23.
+   * Problem: Longest Consecutive Subsequence.
+   * Solution:
+   */
+
+  /**
+   * 24.
+   * Problem: Find the largest subarray with 0 sum.
+   * Solution:
+   */
+
+  /**
+   * 25.
+   * Problem: Given an array of pairs, find all symmetric pairs in it.
+   * Solution:
+   */
+
+  /**
+   * 26.
+   * Problem: Find four elements a, b, c and d in an array such that a+b = c+d.
+   * Solution:
+   */
+
+  /**
+   * 27.
+   * Problem: Check if an array can be divided into pairs whose sum is divisible  by k.
+   * Solution:
+   */
+
+  /**
+   * 28.
+   * Problem: Convert an array to reduced form | Set 1 (Simple and Hashing).
+   * Solution:
+   */
+
+  /**
+   * 29.
+   * Problem: Count pairs with given sum.
+   * Solution:
+   */
+
+  /**
+   * 30.
+   * Problem: Implementing our Own Hash Table with Separate Chaining in Java.
+   * Solution:
+   */
+
+  /**
+   * 31.
+   * Problem: Common elements in all rows of a given matrix.
+   * Solution:
+   */
+
+  /**
+   * 32.
+   * Problem: Cuckoo Hashing  &#8211; Worst case O(1) Lookup!.
+   * Solution:
+   */
+
+  /**
+   * 33.
+   * Problem: Find missing elements of a range.
+   * Solution:
+   */
+
+  /**
+   * 34.
+   * Problem: Pair with given product | Set 1 (Find if any pair exists).
+   * Solution:
+   */
+
+  /**
+   * 35.
+   * Problem: Find smallest range containing elements from k lists.
+   * Solution:
+   */
+
+  /**
+   * 36.
+   * Problem: Find subarray with given sum | Set 2 (Handles Negative Numbers).
+   * Solution:
+   */
+
+  /**
+   * 37.
+   * Problem: Print all subarrays with 0 sum.
+   * Solution:
+   */
+
+  /**
+   * 38.
+   * Problem: Palindrome Substring Queries.
+   * Solution:
+   */
+
+  /**
+   * 39.
+   * Problem: Group Shifted String.
+   * Solution:
+   */
+
+  /**
+   * 40.
+   * Problem: Count maximum points on same line.
+   * Solution:
+   */
+
+  /**
+   * 41.
+   * Problem: Count Substrings with equal number of 0s, 1s and 2s.
+   * Solution:
+   */
+
+  /**
+   * 42.
+   * Problem: Count all pairs with given XOR.
+   * Solution:
+   */
+
+  /**
+   * 43.
+   * Problem: Find pairs with given sum such that elements of pair are in different rows.
+   * Solution:
+   */
+
+  /**
+   * 44.
+   * Problem: Distinct strings with odd and even changes allowed.
+   * Solution:
+   */
+
+  /**
+   * 45.
+   * Problem: Find if there is a pair in root to a leaf path with sum equals to root&#8217;s data.
+   * Solution:
+   */
+
+  /**
+   * 46.
+   * Problem: Minimum delete operations to make all elements of array same.
+   * Solution:
+   */
+
+  /**
+   * 47.
+   * Problem: Check for Palindrome  after every character replacement Query.
+   * Solution:
+   */
+
+  /**
+   * 48.
+   * Problem: Root to leaf paths having equal lengths in a Binary Tree.
+   * Solution:
+   */
+
+  /**
+   * 49.
+   * Problem: Maximum distance between two occurrences of same element in array.
+   * Solution:
+   */
+
+  /**
+   * 50.
+   * Problem: Count pairs whose products exist in array.
+   * Solution:
+   */
+
+  /**
+   * 51.
+   * Problem: Convert to a string that is repetition of a substring of k length.
+   * Solution:
+   */
+
+  /**
+   * 52.
+   * Problem: Subarrays with distinct elements.
+   * Solution:
+   */
+
+  /**
+   * 53.
+   * Problem: Check whether BST contains Dead End or not.
+   * Solution:
+   */
+
+  /**
+   * 54.
+   * Problem: Find all permuted rows of a given row in  a matrix.
+   * Solution:
+   */
+
+  /**
+   * 55.
+   * Problem: Find pair with greatest product in array.
+   * Solution:
+   */
+
+  /**
+   * 56.
+   * Problem: Find pairs in array whose sums already exist in array.
+   * Solution:
+   */
+
+  /**
+   * 57.
+   * Problem: Find elements which are present in first array and not in second.
+   * Solution:
+   */
+
+  /**
+   * 58.
+   * Problem: Find three element from different three arrays such that that a + b + c = sum.
+   * Solution:
+   */
+
+  /**
+   * 59.
+   * Problem: Pairs of Amicable Numbers.
+   * Solution:
+   */
+
+  /**
+   * 60.
+   * Problem: Print a Binary Tree in Vertical Order | Set 3 (Using Level Order Traversal).
+   * Solution:
+   */
+
+  /**
+   * 61.
+   * Problem: Count of index pairs with equal elements in an array.
+   * Solution:
+   */
+
+  /**
+   * 62.
+   * Problem: Make two sets disjoint by removing minimum elements.
+   * Solution:
+   */
+
+  /**
+   * 63.
+   * Problem: Maximum difference between frequency of two elements such that element having greater frequency is also greater.
+   * Solution:
+   */
+
+  /**
+   * 64.
+   * Problem: Range Queries for Frequencies of array elements.
+   * Solution:
+   */
+
+  /**
+   * 65.
+   * Problem: Find all  triplets with zero sum.
+   * Solution:
+   */
+
+  /**
+   * 66.
+   * Problem: Check if two arrays are equal or not.
+   * Solution:
+   */
+
+  /**
+   * 67.
+   * Problem: Given two unsorted arrays, find all pairs whose sum is x.
+   * Solution:
+   */
+
+  /**
+   * 68.
+   * Problem: Group words with same set of characters.
+   * Solution:
+   */
+
+  /**
+   * 69.
+   * Problem: Find all pairs (a,b) and (c,d) in array which satisfy ab = cd.
+   * Solution:
+   */
+
+  /**
+   * 70.
+   * Problem: Find sum of non-repeating (distinct) elements in an array.
+   * Solution:
+   */
+
+  /**
+   * 71.
+   * Problem: Recaman&#8217;s sequence.
+   * Solution:
+   */
+
+  /**
+   * 72.
+   * Problem: How to store a password in database?.
+   * Solution:
+   */
+
+  /**
+   * 73.
+   * Problem: Find the first repeated character in a string.
+   * Solution:
+   */
+
+  /**
+   * 74.
+   * Problem: Count the number of subarrays having a given XOR.
+   * Solution:
+   */
+
+  /**
+   * 75.
+   * Problem: Internal Working of HashMap in Java.
+   * Solution:
+   */
+
+  /**
+   * 76.
+   * Problem: Top 20 Hashing Technique based Interview Questions.
+   * Solution:
+   */
+
+  /**
+   * 77.
+   * Problem: Find duplicates in a given array when elements are not limited to a range.
+   * Solution:
+   */
+
+  /**
+   * 78.
+   * Problem: k-th distinct (or non-repeating) element in an array..
+   * Solution:
+   */
+
+  /**
+   * 79.
+   * Problem: Largest subset whose all elements are Fibonacci numbers.
+   * Solution:
+   */
+
+  /**
+   * 80.
+   * Problem: First element occurring k times in an array.
+   * Solution:
+   */
+
+  /**
+   * 81.
+   * Problem: Count subsets having distinct even numbers.
+   * Solution:
+   */
+
+  /**
+   * 82.
+   * Problem: Maximum difference between first and last indexes of an element in array.
+   * Solution:
+   */
+
+  /**
+   * 83.
+   * Problem: Find distinct elements common to all rows of a matrix.
+   * Solution:
+   */
+
+  /**
+   * 84.
+   * Problem: Union and Intersection of two linked lists | Set-3 (Hashing).
+   * Solution:
+   */
+
+  /**
+   * 85.
+   * Problem: Count subarrays having total distinct elements same as original array.
+   * Solution:
+   */
+
+  /**
+   * 86.
+   * Problem: Count pairs from two sorted arrays whose sum is equal to a given value x.
+   * Solution:
+   */
+
+  /**
+   * 87.
+   * Problem: Find all pairs (a, b) in an array such that a % b = k.
+   * Solution:
+   */
+
+}
