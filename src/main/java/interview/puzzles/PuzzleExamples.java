08187653a9260ed81ca33dc205fc6b3e44f00cd3
Pankaj Thakur <pankajthakur2806@gmail.com>
2017-06-25 22:36:29 +0530

    added code

---
+++ src/main/java/interview/puzzles/PuzzleExamples.java
@@ -0,0 +1,563 @@
+package interview.puzzles;
+
+public class PuzzleExamples {
+
+  /**
+   * 1.
+   * Problem: Puzzle 1 | (How to Measure 45 minutes using two identical wires?).
+   * Solution:
+   */
+
+  /**
+   * 2.
+   * Problem: Puzzle 2 | (Find ages of daughters).
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem: Puzzle 3 | (Calculate total distance travelled by bee).
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem: Puzzle 4 | (Pay an employee using a 7 units gold rod?).
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem: Puzzle 5 | (Finding the poisoned wine).
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem: Puzzle 6 | (Monty Hall problem).
+   * Solution:
+   */
+
+  /**
+   * 7.
+   * Problem: Puzzle 7 | (3 Bulbs and 3 Switches).
+   * Solution:
+   */
+
+  /**
+   * 8.
+   * Problem: Puzzle 8 | (Find the Jar with contaminated pills).
+   * Solution:
+   */
+
+  /**
+   * 9.
+   * Problem: Puzzle 9 | (Find the fastest 3 horses).
+   * Solution:
+   */
+
+  /**
+   * 10.
+   * Problem: Puzzle 10 | (A Man with Medical Condition and 2 Pills).
+   * Solution:
+   */
+
+  /**
+   * 11.
+   * Problem: Puzzle 11 | (1000 Coins and 10 Bags).
+   * Solution:
+   */
+
+  /**
+   * 12.
+   * Problem: Puzzle 12 | (Maximize probability of White Ball).
+   * Solution:
+   */
+
+  /**
+   * 13.
+   * Problem: Puzzle 13 | (100 Prisoners with Red/Black Hats).
+   * Solution:
+   */
+
+  /**
+   * 14.
+   * Problem: Puzzle 14 | (Strategy for a 2 Player Coin Game).
+   * Solution:
+   */
+
+  /**
+   * 15.
+   * Problem: Puzzle 15 | (Camel and Banana Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 16.
+   * Problem: Puzzle 16 | (100 Doors).
+   * Solution:
+   */
+
+  /**
+   * 17.
+   * Problem: Puzzle 17 | (Ratio of Boys and Girls in a Country where people want only boys).
+   * Solution:
+   */
+
+  /**
+   * 18.
+   * Problem: Puzzle 18 | (Torch and Bridge).
+   * Solution:
+   */
+
+  /**
+   * 19.
+   * Problem: Puzzle 19 | (Poison and Rat).
+   * Solution:
+   */
+
+  /**
+   * 20.
+   * Problem: Puzzle 20 | (5 Pirates and 100 Gold Coins).
+   * Solution:
+   */
+
+  /**
+   * 21.
+   * Problem: Puzzle 21 | (3 Ants and Triangle).
+   * Solution:
+   */
+
+  /**
+   * 22.
+   * Problem: Puzzle 22 | (Maximum Chocolates).
+   * Solution:
+   */
+
+  /**
+   * 23.
+   * Problem: Puzzle 23 | (Days of month using 2 dice).
+   * Solution:
+   */
+
+  /**
+   * 24.
+   * Problem: Puzzle 24 | (10 Coins Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 25.
+   * Problem: Puzzle 25 | (Chessboard and dominos).
+   * Solution:
+   */
+
+  /**
+   * 26.
+   * Problem: Puzzle 26 | (Know Average Salary without Disclosing Individual Salaries).
+   * Solution:
+   */
+
+  /**
+   * 27.
+   * Problem: Puzzle 27 | (Hourglasses Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 28.
+   * Problem: Puzzle 28 | (Newspaper Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 29.
+   * Problem: Puzzle 29 | (Car Wheel Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 30.
+   * Problem: Puzzle 30 | (Last Palindrome Date Before 10/02/2001).
+   * Solution:
+   */
+
+  /**
+   * 31.
+   * Problem: Puzzle 31 | (Minimum cut Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 32.
+   * Problem: Puzzle 32| (Completion of Task).
+   * Solution:
+   */
+
+  /**
+   * 33.
+   * Problem: Puzzle 33 | ( Rs 500 Note Puzzle ).
+   * Solution:
+   */
+
+  /**
+   * 34.
+   * Problem: Puzzle 34 | (Prisoner and Policeman Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 35.
+   * Problem: Puzzle | Set 35 (2 Eggs and 100 Floors).
+   * Solution:
+   */
+
+  /**
+   * 36.
+   * Problem: Puzzle 36 | (Matchstick Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 37.
+   * Problem: Puzzle 37 | (Maximum run in cricket).
+   * Solution:
+   */
+
+  /**
+   * 38.
+   * Problem: Puzzle 38 | (Tic Tac Toe Puzzle).
+   * Solution:
+   */
+
+  /**
+   * 39.
+   * Problem: Puzzle 39 | (100 coins puzzle).
+   * Solution:
+   */
+
+  /**
+   * 40.
+   * Problem: Puzzle 40 | (Find missing Row in Excel).
+   * Solution:
+   */
+
+  /**
+   * 41.
+   * Problem: Puzzle 41 | (Guess Color of Hat).
+   * Solution:
+   */
+
+  /**
+   * 42.
+   * Problem: Puzzle 42 | (Placing the numbers).
+   * Solution:
+   */
+
+  /**
+   * 43.
+   * Problem: Puzzle 43 | Muddy Heads.
+   * Solution:
+   */
+
+  /**
+   * 44.
+   * Problem: Puzzle 44 | Girl or Boy.
+   * Solution:
+   */
+
+  /**
+   * 45.
+   * Problem: Puzzle 45 | Gem in Pockets.
+   * Solution:
+   */
+
+  /**
+   * 46.
+   * Problem: Puzzle 46 | Rich or Poor.
+   * Solution:
+   */
+
+  /**
+   * 47.
+   * Problem: Puzzle 47 | Red Hat vs Blue Hat.
+   * Solution:
+   */
+
+  /**
+   * 48.
+   * Problem: Puzzle 48 | Rectangular Cardboard.
+   * Solution:
+   */
+
+  /**
+   * 49.
+   * Problem: Puzzle 49 | King and his Elephants.
+   * Solution:
+   */
+
+  /**
+   * 50.
+   * Problem: Puzzle 50 | The Boat Wreckage.
+   * Solution:
+   */
+
+  /**
+   * 51.
+   * Problem: Puzzle 51| Cheryl’s Birthday Puzzle and Solution.
+   * Solution:
+   */
+
+  /**
+   * 52.
+   * Problem: Puzzle 52 | Boy with Marbles.
+   * Solution:
+   */
+
+  /**
+   * 53.
+   * Problem: Puzzle 53 | The Counterfeit Coin.
+   * Solution:
+   */
+
+  /**
+   * 54.
+   * Problem: Puzzle 54 | Fill the Jug.
+   * Solution:
+   */
+
+  /**
+   * 55.
+   * Problem: Puzzle 55 | Geek and his Friend.
+   * Solution:
+   */
+
+  /**
+   * 56.
+   * Problem: Puzzle 56 | Chinchillas Relations.
+   * Solution:
+   */
+
+  /**
+   * 57.
+   * Problem: Puzzle &#8211; 57| Priests in Temple.
+   * Solution:
+   */
+
+  /**
+   * 58.
+   * Problem: Puzzle -58 | Friends after ages.
+   * Solution:
+   */
+
+  /**
+   * 59.
+   * Problem: Puzzle-59 | Catch the Thief.
+   * Solution:
+   */
+
+  /**
+   * 60.
+   * Problem: Puzzle-60 | Colliding Stars.
+   * Solution:
+   */
+
+  /**
+   * 61.
+   * Problem: Puzzle-61| Cake and my friend.
+   * Solution:
+   */
+
+  /**
+   * 62.
+   * Problem: Puzzle 62 | Gold or Silver.
+   * Solution:
+   */
+
+  /**
+   * 63.
+   * Problem: Puzzle 63 | Paper ball and three friends.
+   * Solution:
+   */
+
+  /**
+   * 64.
+   * Problem: Puzzle 64 | Surround the Villages.
+   * Solution:
+   */
+
+  /**
+   * 65.
+   * Problem: Puzzle 65 | Hats Off.
+   * Solution:
+   */
+
+  /**
+   * 66.
+   * Problem: Puzzle 66 | Walk to Office.
+   * Solution:
+   */
+
+  /**
+   * 67.
+   * Problem: Puzzle 67 | Fit Triangle.
+   * Solution:
+   */
+
+  /**
+   * 68.
+   * Problem: Puzzle 68 | Walking down the hill.
+   * Solution:
+   */
+
+  /**
+   * 69.
+   * Problem: Puzzle 69 |The Number Game.
+   * Solution:
+   */
+
+  /**
+   * 70.
+   * Problem: Puzzle 70 | Tom and Jerry.
+   * Solution:
+   */
+
+  /**
+   * 71.
+   * Problem: Puzzle 71 | Correct Number to save life.
+   * Solution:
+   */
+
+  /**
+   * 72.
+   * Problem: Puzzle 72 | Diseases and Tests.
+   * Solution:
+   */
+
+  /**
+   * 73.
+   * Problem: Puzzle 73 | The Card Game.
+   * Solution:
+   */
+
+  /**
+   * 74.
+   * Problem: Puzzle 74 | Chocolate love.
+   * Solution:
+   */
+
+  /**
+   * 75.
+   * Problem: Puzzle 75 | Prisoner&#8217;s Hat.
+   * Solution:
+   */
+
+  /**
+   * 76.
+   * Problem: Puzzle 76 | Toggling prison cell.
+   * Solution:
+   */
+
+  /**
+   * 77.
+   * Problem: Puzzle 77 | The better choice.
+   * Solution:
+   */
+
+  /**
+   * 78.
+   * Problem: Puzzle 78 | The lost bus Number.
+   * Solution:
+   */
+
+  /**
+   * 79.
+   * Problem: Puzzle 79 | Magical Matrix.
+   * Solution:
+   */
+
+  /**
+   * 80.
+   * Problem: Puzzle 80 | Tic Tac Toe revisited.
+   * Solution:
+   */
+
+  /**
+   * 81.
+   * Problem: Puzzle 81 | 100 people in a circle with gun puzzle.
+   * Solution:
+   */
+
+  /**
+   * 82.
+   * Problem: Puzzle 82 | Different Pairings.
+   * Solution:
+   */
+
+  /**
+   * 83.
+   * Problem: Puzzle 83 | Interesting MCQ.
+   * Solution:
+   */
+
+  /**
+   * 84.
+   * Problem: Puzzle 84 | Inverted Matrix.
+   * Solution:
+   */
+
+  /**
+   * 85.
+   * Problem: Puzzle 85 | Chain Link Puzzle.
+   * Solution:
+   */
+
+  /**
+   * 86.
+   * Problem: Measuring 6L water from 4L and 9L buckets.
+   * Solution:
+   */
+
+  /**
+   * 87.
+   * Problem: Missionaries and Cannibals.
+   * Solution:
+   */
+
+  /**
+   * 88.
+   * Problem: Four People on a Rickety Bridge.
+   * Solution:
+   */
+
+  /**
+   * 89.
+   * Problem: The Blue – eyed Island puzzle.
+   * Solution:
+   */
+
+  /**
+   * 90.
+   * Problem: Equilateral Triangles using Matchsticks.
+   * Solution:
+   */
+
+  /**
+   * 91.
+   * Problem: Puzzle  | (4 Matchstick Problem).
+   * Solution:
+   */
+
+  /**
+   * 92.
+   * Problem: Puzzle | Weight of Heavy Ball.
+   * Solution:
+   */
+
+  /**
+   * 93.
+   * Problem: Puzzle | Camels and Travelers.
+   * Solution:
+   */
+
+}
