994c1c66b946ec9072afe5bcadb2329527665827
abhay0609 <abhay0609@gmail.com>
2017-06-27 00:14:03 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1272,12 +1272,8 @@ public class DPExamples {
 
   int maxDotProductBottomUp(int[] a, int[] b) {
     int[][] dp = new int[a.length + 1][b.length + 1];
-    for (int i = 1;  i <= a.length; i++) {
-      for (int j = 1; j <= b.length; j++) {
-        dp[i][j] = Math.max(a[i - 1] * b[j - 1] + dp[i - 1][j - 1],
-                dp[i - 1][j]);
-      }
-    }
+    for (int i = 1;  i <= a.length; i++)
+      for (int j = 1; j <= b.length; j++) dp[i][j] = Math.max(a[i - 1] * b[j - 1] + dp[i - 1][j - 1], dp[i - 1][j]);
     return dp[a.length][b.length];
   }
 
