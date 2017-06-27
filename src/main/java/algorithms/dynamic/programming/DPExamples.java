4e622363e5606ec1733ac9c40a777e173754b953
abhay0609 <abhay0609@gmail.com>
2017-06-27 17:21:44 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1606,7 +1606,12 @@ public class DPExamples {
    * -->// minimum number of Insertions
    * minInsert = n - len
    */
-  int minInsertionDeletion(String str1, String str2, int m , int n) {
+  int[] minInsertionDeletion(String str1, String str2, int m , int n) {
+    int[] result = new int[2];
+    int len = new DPExamples().LISBottomUp(str1, str2);
+    result[0] = m - len;
+    result[1] = n - len;
+    return result;
   }
 
   /**
