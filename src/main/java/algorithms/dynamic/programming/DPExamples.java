8185dcdd3e188449e0dacd9ebda79bd2cea9cc80
abhay0609 <abhay0609@gmail.com>
2017-06-27 10:43:46 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1262,11 +1262,11 @@ public class DPExamples {
    * We multiply A[i] and B[j] and add to product (We include A[i]).
    * We exclude A[i] from product (In other words, we insert 0 at current position in B[])
    */
-  int maxDotProduct(int[] a, int[] b, int aLength, int bLength) {
-    if (aLength == 0 || bLength == 0) return 0;
+  int maxDotProduct(int[] a, int[] b, int m, int n) {
+    if (m == 0 || n == 0) return 0;
     else
-      return Math.max( a[aLength - 1] * b [bLength - 1] + maxDotProduct(a, b, aLength - 1, bLength - 1),
-              maxDotProduct(a, b, aLength - 1, bLength));
+      return Math.max( a[m - 1] * b [n - 1] + maxDotProduct(a, b, m - 1, n - 1),
+              maxDotProduct(a, b, m - 1, n));
 
   }
 
