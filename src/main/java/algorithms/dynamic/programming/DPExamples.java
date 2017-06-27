9b5f2c5a75ed5445d50780d434b6020a05fd33f9
abhay0609 <abhay0609@gmail.com>
2017-06-27 17:10:04 +0530

    refactoring

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -193,10 +193,9 @@ public class DPExamples {
 
   int LISBottomUp(String str1, String str2) {
     int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-    for (int i = 0; i <= str1.length(); i++) {
-      for (int j= 0; j <= str2.length(); j++) {
-        if (i == 0 || j == 0 ) dp[i][j] = 0;
-        else if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+    for (int i = 1; i <= str1.length(); i++) {
+      for (int j= 1; j <= str2.length(); j++) {
+        if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
         else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
       }
     }
