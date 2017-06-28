4ed3bf91dca12240abe314c3ff8e5da06cf7aa9c
abhay0609 <abhay0609@gmail.com>
2017-06-28 11:54:35 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1438,6 +1438,15 @@ public class DPExamples {
   /**
    * 175.
    * Problem: Count of strings that can be formed using a, b and c under given constraints.
+   * Given a length n, count the number of strings of length n that can be made using ‘a’, ‘b’ and ‘c’ with at-most one ‘
+   * b’ and two ‘c’s allowed.
+   * Input : n = 3
+   * Output : 19
+   * Below strings follow given constraints:
+   * aaa aab aac aba abc aca acb acc baa
+   * bac bca bcc caa cab cac cba cbc cca ccb
+   * Input  : n = 4
+   * Output : 39
    * Solution:
    */
 
