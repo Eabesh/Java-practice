f17179af37f7dbb6d43b3053a321c15b3714f0e3
abhay0609 <abhay0609@gmail.com>
2017-06-28 11:18:35 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1639,6 +1639,14 @@ public class DPExamples {
   /**
    * 201.
    * Problem: Minimum insertions to sort an array.
+   * Given an array of integer numbers, we need to sort this array in a minimum number of steps where in one step we can
+   * insert any array element from its position to any other position.
+   * Input  : arr[] = [2, 3, 5, 1, 4, 7, 6]
+   * Output : 3
+   * We can sort above array in 3 insertionsteps as shown below, 1 before array value 24 before array value 56 before
+   * array value 7
+   * Input : arr[] = {4, 6, 5, 1}
+   * Output : 2
    * Solution:
    */
 
