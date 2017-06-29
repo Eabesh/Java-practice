0972081cf978b633ec28c469a1293e9dc687a17f
abhay0609 <abhay0609@gmail.com>
2017-06-29 23:15:16 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1376,6 +1376,11 @@ public class DPExamples {
   /**
    * 168.
    * Problem: Probability of getting at least K heads in N tosses of Coins.
+   * Given N number of coins, the task is to find probability of getting at least K number of heads after tossing all
+   * the N coins simultaneously.
+   * Suppose we have 3 unbiased coins and we have to find the probability of getting at least 2 heads, so there are
+   * 23 = 8 ways to toss these coins, i.e., HHH, HHT, HTH, HTT, THH, THT, TTH, TTT Out of which there are 4 set which
+   * contain at least 2 Heads i.e., HHH, HHT, HH, THH So the probability is 4/8 or 0.5
    * Solution:
    */
 
