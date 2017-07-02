695032e72c5b3b606f276f7d0a303e240632110d
abhay0609 <abhay0609@gmail.com>
2017-07-02 12:13:56 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1416,22 +1416,14 @@ public class DPExamples {
    * Problem: Find all distinct subset (or subsequence) sums of an array
    * Given a set of integers, find distinct sum that can be generated from the subsets of the given sets and print them
    * in an increasing order. It is given that sum of array elements is small.
-   *
-   Input  : arr[] = {1, 2, 3}
-   Output : 0 1 2 3 4 5 6
-   Distinct subsets of given set are
-   {}, {1}, {2}, {3}, {1,2}, {2,3},
-   {1,3} and {1,2,3}.  Sums of these
-   subsets are 0, 1, 2, 3, 3, 5, 4, 6
-   After removing duplicates, we get
-   0, 1, 2, 3, 4, 5, 6
-
-   Input : arr[] = {2, 3, 4, 5, 6}
-   Output : 0 2 3 4 5 6 7 8 9 10 11 12
-   13 14 15 16 17 18 20
-
-   Input : arr[] = {20, 30, 50}
-   Output : 0 20 30 50 70 80 100
+   * Input  : arr[] = {1, 2, 3}
+   * Output : 0 1 2 3 4 5 6
+   * Distinct subsets of given set are {}, {1}, {2}, {3}, {1,2}, {2,3}, {1,3} and {1,2,3}.  Sums of these subsets are
+   * 0, 1, 2, 3, 3, 5, 4, 6 After removing duplicates, we get 0, 1, 2, 3, 4, 5, 6
+   * Input : arr[] = {2, 3, 4, 5, 6}
+   * Output : 0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 20
+   * Input : arr[] = {20, 30, 50}
+   * Output : 0 20 30 50 70 80 100
    * Solution:
    */
 
