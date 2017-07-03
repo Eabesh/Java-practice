eeedaba018dcf49cfa6b179c3af55a389b0af0a0
abhay0609 <abhay0609@gmail.com>
2017-07-04 01:59:01 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -763,6 +763,33 @@ public class DPExamples {
   /**
    * 60.
    * Problem: How to print maximum number of A’s using given four keys
+   * This is a famous interview question asked in Google, Paytm and many other company interviews.
+   * Imagine you have a special keyboard with the following keys:
+   * Key 1:  Prints 'A' on screen
+   * Key 2: (Ctrl-A): Select screen
+   * Key 3: (Ctrl-C): Copy selection to buffer
+   * Key 4: (Ctrl-V): Print buffer on screen appending it after what has already been printed.
+   * If you can only press the keyboard for N times (with the above four keys), write a program to produce maximum
+   * numbers of A's. That is to say, the input parameter is N (No. of keys that you can press), the
+   * output is M (No. of As that you can produce).
+   * Input:  N = 3
+   Output: 3
+   We can at most get 3 A's on screen by pressing
+   following key sequence.
+   A, A, A
+
+   Input:  N = 7
+   Output: 9
+   We can at most get 9 A's on screen by pressing
+   following key sequence.
+   A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
+
+   Input:  N = 11
+   Output: 27
+   We can at most get 27 A's on screen by pressing
+   following key sequence.
+   A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V, Ctrl A,
+   Ctrl C, Ctrl V, Ctrl V
    * Solution:
    */
 
