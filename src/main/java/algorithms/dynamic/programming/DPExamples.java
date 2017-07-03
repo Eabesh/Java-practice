6d5654e2f5d42fdcd34a9fe0ff380c5ac9e304fa
abhay0609 <abhay0609@gmail.com>
2017-07-03 15:35:56 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -618,6 +618,14 @@ public class DPExamples {
   /**
    * 49.
    * Problem: Count number of binary strings without consecutive 1’s
+   * Given a positive integer N, count all possible distinct binary strings of length N such that there are no
+   * consecutive 1’s.
+   * Input:  N = 2
+   * Output: 3
+   * // The 3 strings are 00, 01, 10
+   * Input: N = 3
+   * Output: 5
+   * // The 5 strings are 000, 001, 010, 100, 101
    * Solution:
    */
 
