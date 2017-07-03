778af2b969851ee5bb59ff8aec0f4114242ba103
abhay0609 <abhay0609@gmail.com>
2017-07-03 19:44:56 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -669,7 +669,6 @@ public class DPExamples {
   }
 
 
-
   /**
    * 52.
    * Problem: Shortest path with exactly k edges in a directed and weighted graph
@@ -704,8 +703,23 @@ public class DPExamples {
   /**
    * 57.
    * Problem: Count number of ways to reach a given score in a game
+   * Consider a game where a player can score 3 or 5 or 10 points in a move. Given a total score n, find number of ways
+   * to reach the given score.
+   * Input: n = 20
+   * Output: 4
+   * There are following 4 ways to reach 20
+   * (10, 10)
+   * (5, 5, 10)
+   * (5, 5, 5, 5)
+   * (3, 3, 3, 3, 3, 5)
+   * Input: n = 13
+   * Output: 2
+   * There are following 2 ways to reach 13
+   * (3, 5, 5)
+   * (3, 10)
    * Solution:
    */
+
 
   /**
    * 58.
