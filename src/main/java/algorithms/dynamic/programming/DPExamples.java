b116b0701a91e866e416a6bfea442e78dc91d07c
abhay0609 <abhay0609@gmail.com>
2017-07-04 22:39:12 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -975,6 +975,16 @@ public class DPExamples {
   /**
    * 70.
    * Problem: Total number of non-decreasing numbers with n digits
+   * A number is non-decreasing if every digit (except the first one) is greater than or equal to previous digit. For
+   * example, 223, 4455567, 899, are non-decreasing numbers.
+   * So, given the number of digits n, you are required to find the count of total non-decreasing numbers with n digits.
+   * Examples:
+   * Input:  n = 1
+   * Output: count  = 10
+   * Input:  n = 2
+   * Output: count  = 55
+   * Input:  n = 3
+   * Output: count  = 220
    * Solution:
    */
 
