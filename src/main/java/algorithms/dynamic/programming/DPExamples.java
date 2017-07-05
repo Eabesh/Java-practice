836c557f94be85b590a5c848fb32475d68b7079d
abhay0609 <abhay0609@gmail.com>
2017-07-05 18:00:54 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -236,6 +236,9 @@ public class DPExamples {
   /**
    * 12.
    * Problem: Minimum number of jumps to reach end
+   * Given an array of integers where each element represents the max number of steps that can be made forward from that
+   * element.Write a function to return the minimum number of jumps to reach the end of the array (starting from the
+   * first element). If an element is 0, then cannot move through that element.
    * Solution:
    */
 
