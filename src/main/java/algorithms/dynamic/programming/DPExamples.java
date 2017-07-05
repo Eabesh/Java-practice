d574e4c228cce551ca21c6a590efb0e624e15f0a
abhay0609 <abhay0609@gmail.com>
2017-07-05 07:41:21 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -1642,10 +1642,6 @@ public class DPExamples {
    * 143.
    * Problem: Maximum sum subarray removing at most one element
    * Given an array, we need to find maximum sum subarray, removing one element is also allowed to get the maximum sum.
-   * Input  : arr[] = {1, 2, 3, -4, 5}
-   * Output : 11
-   * Explanation : We can get maximum sum subarray by removing -4.
-   * Solution:
    */
   int maxSubArraySum(int[] array) {
     int[] forWardSum = new int[array.length], backWardSum = new int[array.length];
