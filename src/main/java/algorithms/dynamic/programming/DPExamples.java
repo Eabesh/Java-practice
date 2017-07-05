977051bd7a456fbee0a521ff30f66ab0b13b3ad9
abhay0609 <abhay0609@gmail.com>
2017-07-05 07:32:08 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -62,14 +62,6 @@ public class DPExamples {
    * Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
    * shows the first 11 ugly numbers. By convention, 1 is included.
    * Given a number n, the task is to find n’th Ugly number.
-   * Input  : n = 7
-   * Output : 8
-   * Input  : n = 10
-   * Output : 12
-   * Input  : n = 15
-   * Output : 24
-   * Input  : n = 150
-   * Output : 5832
    * Solution:
    */
 
@@ -428,12 +420,12 @@ public class DPExamples {
    * Problem: Dynamic Programming | Set 24 (Optimal Binary Search Tree)
    * Solution:
    */
+
   /**
    * 32.
    * Problem: Dynamic Programming | Set 25 (Subset Sum Problem)
-   * Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
-   * Examples: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
-   * Output:  True  //There is a subset (4, 5) with sum 9.
+   * Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum
+   * equal to given sum.
    * Solution:
    */
   boolean isSubsetSum(int[] set, int setLen, int sum) {
