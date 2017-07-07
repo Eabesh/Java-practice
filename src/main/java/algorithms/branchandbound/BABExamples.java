e4e6649e520f7ffea70f5f08c60ea559d14ffb8c
abhay0609 <abhay0609@gmail.com>
2017-07-07 10:57:30 +0530

    added problems

---
+++ src/main/java/algorithms/branchandbound/BABExamples.java
@@ -0,0 +1,43 @@
+package algorithms.branchandbound;
+
+/**
+ * Created by abhay on 07/07/17.
+ */
+public class BABExamples {
+  /**
+   * 1.
+   * Problem: Branch and Bound | Set 1 (Introduction with 0/1 Knapsack).
+   * Solution:
+   */
+
+  /**
+   * 2.
+   * Problem: Branch and Bound | Set 2 (Implementation of 0/1 Knapsack).
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem: Branch and Bound | Set 3 (8 puzzle Problem).
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem: Branch and Bound | Set 5 (N Queen Problem).
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem: Branch And Bound | Set 4 (Job Assignment Problem).
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem: Branch And Bound | Set 6 (Traveling Salesman Problem).
+   * Solution:
+   */
+
+}
---
+++ src/main/java/algorithms/geometric/GeometricExamples.java
@@ -0,0 +1,389 @@
+package algorithms.geometric;
+
+/**
+ * Created by abhay on 07/07/17.
+ */
+public class GeometricExamples {
+
+
+  /**
+   * 1.
+   * Problem: Check whether a given point lies inside a triangle or not.
+   * Solution:
+   */
+
+  /**
+   * 2.
+   * Problem: Divide and Conquer | Set 2 (Closest Pair of Points).
+   * Solution:
+   */
+
+  /**
+   * 3.
+   * Problem: How to check if two given line segments intersect?.
+   * Solution:
+   */
+
+  /**
+   * 4.
+   * Problem: How to check if a given point lies inside or outside a polygon?.
+   * Solution:
+   */
+
+  /**
+   * 5.
+   * Problem: Convex Hull | Set 1 (Jarvis&#8217;s Algorithm or Wrapping).
+   * Solution:
+   */
+
+  /**
+   * 6.
+   * Problem: Convex Hull | Set 2 (Graham Scan).
+   * Solution:
+   */
+
+  /**
+   * 7.
+   * Problem: Given n line segments, find if any two segments intersect.
+   * Solution:
+   */
+
+  /**
+   * 8.
+   * Problem: Closest Pair of Points |  O(nlogn) Implementation.
+   * Solution:
+   */
+
+  /**
+   * 9.
+   * Problem: Find if two rectangles overlap.
+   * Solution:
+   */
+
+  /**
+   * 10.
+   * Problem: C program to find area of a triangle.
+   * Solution:
+   */
+
+  /**
+   * 11.
+   * Problem: program to find area of a circle.
+   * Solution:
+   */
+
+  /**
+   * 12.
+   * Problem: Minimum Cost Polygon Triangulation.
+   * Solution:
+   */
+
+  /**
+   * 13.
+   * Problem: How to check if given four points form a square.
+   * Solution:
+   */
+
+  /**
+   * 14.
+   * Problem: Find Simple Closed Path for a given set of points.
+   * Solution:
+   */
+
+  /**
+   * 15.
+   * Problem: Orientation of 3 ordered points.
+   * Solution:
+   */
+
+  /**
+   * 16.
+   * Problem: Count Integral points inside a Triangle.
+   * Solution:
+   */
+
+  /**
+   * 17.
+   * Problem: Number of Integral Points between Two Points.
+   * Solution:
+   */
+
+  /**
+   * 18.
+   * Problem: Area of a polygon with given n ordered vertices.
+   * Solution:
+   */
+
+  /**
+   * 19.
+   * Problem: Non-crossing lines to connect points in a circle.
+   * Solution:
+   */
+
+  /**
+   * 20.
+   * Problem: Count maximum points on same line.
+   * Solution:
+   */
+
+  /**
+   * 21.
+   * Problem: Klee&#8217;s Algorithm (Length Of Union Of Segments of a line).
+   * Solution:
+   */
+
+  /**
+   * 22.
+   * Problem: Optimum location of point to minimize total distance.
+   * Solution:
+   */
+
+  /**
+   * 23.
+   * Problem: Classify a triangle.
+   * Solution:
+   */
+
+  /**
+   * 24.
+   * Problem: Maximum height when coins are arranged in a triangle.
+   * Solution:
+   */
+
+  /**
+   * 25.
+   * Problem: n&#8217;th Pentagonal Number.
+   * Solution:
+   */
+
+  /**
+   * 26.
+   * Problem: Circle and Lattice Points.
+   * Solution:
+   */
+
+  /**
+   * 27.
+   * Problem: Find all sides of a right angled triangle from given hypotenuse and area | Set 1.
+   * Solution:
+   */
+
+  /**
+   * 28.
+   * Problem: Line Clipping | Set 1 (Cohen–Sutherland Algorithm).
+   * Solution:
+   */
+
+  /**
+   * 29.
+   * Problem: Find perimeter of shapes formed with 1s in binary matrix.
+   * Solution:
+   */
+
+  /**
+   * 30.
+   * Problem: Count of parallelograms in a plane.
+   * Solution:
+   */
+
+  /**
+   * 31.
+   * Problem: Queries on count of points lie inside a circle.
+   * Solution:
+   */
+
+  /**
+   * 32.
+   * Problem: Paper Cut into Minimum Number of Squares.
+   * Solution:
+   */
+
+  /**
+   * 33.
+   * Problem: Check whether a point exists in circle sector or not..
+   * Solution:
+   */
+
+  /**
+   * 34.
+   * Problem: Count of acute, obtuse and right triangles with given sides.
+   * Solution:
+   */
+
+  /**
+   * 35.
+   * Problem: Find an Integer point on a line segment with given two ends.
+   * Solution:
+   */
+
+  /**
+   * 36.
+   * Problem: Minimum distance to travel to cover all intervals.
+   * Solution:
+   */
+
+  /**
+   * 37.
+   * Problem: Check if four segments form a rectangle.
+   * Solution:
+   */
+
+  /**
+   * 38.
+   * Problem: Pizza cut problem (Or Circle Division by Lines).
+   * Solution:
+   */
+
+  /**
+   * 39.
+   * Problem: Maximum number of 2&#215;2 squares that can be fit inside a right isosceles triangle.
+   * Solution:
+   */
+
+  /**
+   * 40.
+   * Problem: Minimum lines to cover all points.
+   * Solution:
+   */
+
+  /**
+   * 41.
+   * Problem: Check if right triangle possible from given area and hypotenuse.
+   * Solution:
+   */
+
+  /**
+   * 42.
+   * Problem: Minimum block jumps to reach destination.
+   * Solution:
+   */
+
+  /**
+   * 43.
+   * Problem: Quickhull Algorithm for Convex Hull.
+   * Solution:
+   */
+
+  /**
+   * 44.
+   * Problem: Polygon Clipping | Sutherland–Hodgman Algorithm.
+   * Solution:
+   */
+
+  /**
+   * 45.
+   * Problem: Triangle with no point inside.
+   * Solution:
+   */
+
+  /**
+   * 46.
+   * Problem: Find all angles of a given triangle.
+   * Solution:
+   */
+
+  /**
+   * 47.
+   * Problem: Tangents between two Convex Polygons.
+   * Solution:
+   */
+
+  /**
+   * 48.
+   * Problem: Convex Hull (Simple Divide and Conquer Algorithm).
+   * Solution:
+   */
+
+  /**
+   * 49.
+   * Problem: Dynamic Convex hull | Adding Points to an Existing Convex Hull.
+   * Solution:
+   */
+
+  /**
+   * 50.
+   * Problem: Minimum revolutions to move center of a circle to a target.
+   * Solution:
+   */
+
+  /**
+   * 51.
+   * Problem: Angular Sweep (Maximum points that can be enclosed in a circle of given radius).
+   * Solution:
+   */
+
+  /**
+   * 52.
+   * Problem: Deleting points from Convex Hull.
+   * Solution:
+   */
+
+  /**
+   * 53.
+   * Problem: Regular polygon using only 1s in a binary numbered circle.
+   * Solution:
+   */
+
+  /**
+   * 54.
+   * Problem: Program for Point of Intersection of Two Lines.
+   * Solution:
+   */
+
+  /**
+   * 55.
+   * Problem: Represent a given set of points by the best possible straight line.
+   * Solution:
+   */
+
+  /**
+   * 56.
+   * Problem: Find number of diagonals in n sided convex polygon.
+   * Solution:
+   */
+
+  /**
+   * 57.
+   * Problem: Find Corners of Rectangle using mid points.
+   * Solution:
+   */
+
+  /**
+   * 58.
+   * Problem: Program to find line passing through 2 Points.
+   * Solution:
+   */
+
+  /**
+   * 59.
+   * Problem: Program to find Circumcenter of a Triangle.
+   * Solution:
+   */
+
+  /**
+   * 60.
+   * Problem: Reflection of a point about a line in C++.
+   * Solution:
+   */
+
+  /**
+   * 61.
+   * Problem: Rotation of a point about another point in C++.
+   * Solution:
+   */
+
+  /**
+   * 62.
+   * Problem: Find points at a given distance on a line of given slope.
+   * Solution:
+   */
+
+  /**
+   * 63.
+   * Problem: Find the Missing Point of Parallelogram.
+   * Solution:
+   */
+
+
+
+}
