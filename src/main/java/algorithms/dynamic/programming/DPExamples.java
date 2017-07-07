3ef16aca7fa687d8a13f88a25edc054634709a5f
abhay0609 <abhay0609@gmail.com>
2017-07-07 13:49:23 +0530

    added problems

--- src/main/java/algorithms/dynamic/programming/DPExamples.java
+++ src/main/java/algorithms/dynamic/programming/DPExamples.java
@@ -114,9 +114,8 @@ public class DPExamples {
   /**
    * 6.
    * Problem: Dynamic Programming | Set 2 (Optimal Substructure Property)
-   * 2) Optimal Substructure: A given problems has Optimal Substructure Property if
-   * optimal solution of the given problem can be obtained by using optimal solutions
-   * of its subproblems.
+   * 2) Optimal Substructure: A given problems has Optimal Substructure Property if optimal solution of the given
+   * problem can be obtained by using optimal solutions of its sub-problems.
    */
 
   /**
