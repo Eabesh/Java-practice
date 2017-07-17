2397fbcb18c7da237ea22eb5690c8bc2f8e45d86
abhay0609 <abhay0609@gmail.com>
2017-07-17 23:52:41 +0530

    added problems

--- src/main/java/algorithms/backtracking/BackTrackingExamples.java
+++ src/main/java/algorithms/backtracking/BackTrackingExamples.java
@@ -133,7 +133,9 @@ public class BackTrackingExamples {
   /**
    * 13.
    * Problem: Fill two instances of all numbers from 1 to n in a specific way.
-   * Solution:
+   * Given a number n, create an array of size 2n such that the array contains 2 instances of every number from 1 to n,
+   * and the number of elements between two instances of a number i is equal to i. If such a configuration is not
+   * possible, then print the same.
    */
 
   /**
