42badce3ff9a85584ea84049635bffdb07e17809
abhay0609 <abhay0609@gmail.com>
2017-08-17 10:43:30 +0530

    added problems

--- src/main/java/data/structures/graph/GraphExamples.java
+++ src/main/java/data/structures/graph/GraphExamples.java
@@ -7,45 +7,6 @@ import java.util.*;
 
 public class GraphExamples {
 
-  /**
-   * Given a matrix of 0's and 1's find the count of biggest area covered by adjacent 1's.
-   * Hint: We can use DFS here.
-   */
-  int maxConnectedCell(int[][] grid) {
-    int m = grid.length;
-    int n = grid.length;
-    boolean[][] isVisited = new boolean[m][n];
-
-    int max = 0;
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (!isVisited[i][j] && grid[i][j] == 1)
-          max = Math.max(max, dfs(grid, i, j, isVisited));
-      }
-    }
-
-    return max;
-  }
-
-  boolean isValidMove(int[][] grid, int x, int y, boolean[][] isVisited) {
-    return (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1 && !isVisited[x][y]);
-  }
-
-  int dfs(int[][] grid, int x, int y, boolean[][] isVisited) {
-    if (!isValidMove(grid, x, y, isVisited)) return 0;
-    else {
-      isVisited[x][y] = true;
-      return 1 +
-              dfs(grid, x, y + 1, isVisited) +
-              dfs(grid, x, y - 1, isVisited) +
-              dfs(grid, x + 1, y, isVisited) +
-              dfs(grid, x - 1, y, isVisited) +
-              dfs(grid, x + 1, y + 1, isVisited) +
-              dfs(grid, x + 1, y - 1, isVisited) +
-              dfs(grid, x - 1, y + 1, isVisited) +
-              dfs(grid, x - 1, y - 1, isVisited);
-    }
-  }
 
   /**
    * 1.
@@ -246,7 +207,7 @@ public class GraphExamples {
 
   /**
    * 17.
-   * Problem: Greedy Algorithms | Set 7 (Dijkstra&#8217;s shortest path algorithm).
+   * Problem: Greedy Algorithms | Set 7 (Dijkstra's shortest path algorithm).
    * Solution:
    */
 
@@ -1168,5 +1129,52 @@ public class GraphExamples {
    * Problem: Clone a Directed Acyclic Graph.
    * Solution:
    */
+
+
+
+
+
+
+
+  /**
+   * Given a matrix of 0's and 1's find the count of biggest area covered by adjacent 1's.
+   * Hint: We can use DFS here.
+   */
+  int maxConnectedCell(int[][] grid) {
+    int m = grid.length;
+    int n = grid.length;
+    boolean[][] isVisited = new boolean[m][n];
+
+    int max = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (!isVisited[i][j] && grid[i][j] == 1)
+          max = Math.max(max, dfs(grid, i, j, isVisited));
+      }
+    }
+
+    return max;
+  }
+
+  boolean isValidMove(int[][] grid, int x, int y, boolean[][] isVisited) {
+    return (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1 && !isVisited[x][y]);
+  }
+
+  int dfs(int[][] grid, int x, int y, boolean[][] isVisited) {
+    if (!isValidMove(grid, x, y, isVisited)) return 0;
+    else {
+      isVisited[x][y] = true;
+      return 1 +
+              dfs(grid, x, y + 1, isVisited) +
+              dfs(grid, x, y - 1, isVisited) +
+              dfs(grid, x + 1, y, isVisited) +
+              dfs(grid, x - 1, y, isVisited) +
+              dfs(grid, x + 1, y + 1, isVisited) +
+              dfs(grid, x + 1, y - 1, isVisited) +
+              dfs(grid, x - 1, y + 1, isVisited) +
+              dfs(grid, x - 1, y - 1, isVisited);
+    }
+  }
+
 
 }
