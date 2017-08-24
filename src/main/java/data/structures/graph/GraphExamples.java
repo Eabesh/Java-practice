ce1be00d006adf1cc928343e3eb3497cf88fe001
abhay0609 <abhay0609@gmail.com>
2017-08-24 23:34:20 +0530

    added problems

--- src/main/java/data/structures/graph/GraphExamples.java
+++ src/main/java/data/structures/graph/GraphExamples.java
@@ -1071,7 +1071,6 @@ public class GraphExamples {
    * Consider a matrix with rows and columns, where each cell contains either a ‘0’ or a ‘1’ and any cell containing a 1
    * is called a filled cell. Two cells are said to be connected if they are adjacent to each other horizontally, vertically,
    * or diagonally .If one or more filled cells are also connected, they form a region. find the length of the largest region.
-   * Solution:
    */
   int findLargestRegion(int[][] mat) {
     boolean[][] visited = new boolean[mat.length][mat[0].length];
