83d13698550ff2e10e2786ab9aa25a24944ca849
abhay0609 <abhay0609@gmail.com>
2018-01-09 11:45:19 +0530

    added problems

--- src/main/java/data/structures/binarytrees/TreeExamples.java
+++ src/main/java/data/structures/binarytrees/TreeExamples.java
@@ -1049,8 +1049,33 @@ public class TreeExamples {
 
   /**
    * 127.
-   * Problem: BFS vs DFS for Binary Tree.
-   * Solution:
+   * Problem: BFS vs DFS for Binary Tree
+   * Solution: A Tree is typically traversed in two ways:
+   * Breadth First Traversal (Or Level Order Traversal)
+   * Depth First Traversals
+   * Inorder Traversal (Left-Root-Right)
+   * Preorder Traversal (Root-Left-Right)
+   * Postorder Traversal (Left-Right-Root)
+   *
+   * Is there any difference in terms of Time Complexity?
+   * All four traversals require O(n) time as they visit every node exactly once.
+   * Is there any difference in terms of Extra Space.
+   * There is difference in terms of extra space required.
+   * Extra Space required for Level Order Traversal is O(w) where w is maximum width of Binary Tree. In level order traversal, queue one by one stores nodes of different level.
+   * Extra Space required for Depth First Traversals is O(h) where h is maximum height of Binary Tree. In Depth First Traversals, stack (or function call stack) stores all ancestors of a node.
+   * Maximum Width of a Binary Tree at depth (or height) h can be 2h where h starts from 0. So the maximum number of nodes can be at the last level. And worst case occurs when Binary Tree is a perfect Binary Tree with numbers of nodes like 1, 3, 7, 15, …etc. In worst case, value of 2h is Ceil(n/2).
+   *
+   * Height for a Balanced Binary Tree is O(Log n). Worst case occurs for skewed tree and worst case height becomes O(n).
+   *
+   * So in worst case extra space required is O(n) for both. But worst cases occur for different types of trees.
+   * How to Pick One?
+   * Extra Space can be one factor (Explained above.
+   *
+   * Depth First Traversals are typically recursive and recursive code requires function call overheads
+   *
+   * The most important points is, BFS starts visiting nodes from root while DFS starts visiting nodes from leaves.
+   * So if our problem is to search something that is more likely to closer to root, we would prefer BFS.
+   * And if the target node is close to a leaf, we would prefer DFS.
    */
 
   /**
@@ -3413,38 +3438,6 @@ public class TreeExamples {
   }
 
 
-  /**
-   * 114.
-   * Problem: BFS vs DFS for Binary Tree
-
-   * Solution: A Tree is typically traversed in two ways:
-   * Breadth First Traversal (Or Level Order Traversal)
-   * Depth First Traversals
-   * Inorder Traversal (Left-Root-Right)
-   * Preorder Traversal (Root-Left-Right)
-   * Postorder Traversal (Left-Right-Root)
-   *
-   *
-   * Is there any difference in terms of Time Complexity?
-   * All four traversals require O(n) time as they visit every node exactly once.
-   * Is there any difference in terms of Extra Space.
-   * There is difference in terms of extra space required.
-   * Extra Space required for Level Order Traversal is O(w) where w is maximum width of Binary Tree. In level order traversal, queue one by one stores nodes of different level.
-   * Extra Space required for Depth First Traversals is O(h) where h is maximum height of Binary Tree. In Depth First Traversals, stack (or function call stack) stores all ancestors of a node.
-   * Maximum Width of a Binary Tree at depth (or height) h can be 2h where h starts from 0. So the maximum number of nodes can be at the last level. And worst case occurs when Binary Tree is a perfect Binary Tree with numbers of nodes like 1, 3, 7, 15, …etc. In worst case, value of 2h is Ceil(n/2).
-   *
-   * Height for a Balanced Binary Tree is O(Log n). Worst case occurs for skewed tree and worst case height becomes O(n).
-   *
-   * So in worst case extra space required is O(n) for both. But worst cases occur for different types of trees.
-   * How to Pick One?
-   * Extra Space can be one factor (Explained above.
-   *
-   * Depth First Traversals are typically recursive and recursive code requires function call overheads
-   *
-   * The most important points is, BFS starts visiting nodes from root while DFS starts visiting nodes from leaves.
-   * So if our problem is to search something that is more likely to closer to root, we would prefer BFS.
-   * And if the target node is close to a leaf, we would prefer DFS.
-   */
 
 
   /**
